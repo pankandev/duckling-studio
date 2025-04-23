8eaa0e5722a61f5df27e472e7ee2282e4c5aea7d
capaths <pcampaa@gmail.com>
2025-04-23 16:03:16 -0400

    chore: add mypy type checking

--- apps/mltasks/services/db.py
+++ apps/mltasks/services/db.py
@@ -1,7 +1,8 @@
 import os
+from typing import Any, Generator
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost:5433/postgres")
 
@@ -10,7 +11,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db():
+def get_db() -> Generator[Session, Any, None]:
     db = SessionLocal()
     try:
         yield db
