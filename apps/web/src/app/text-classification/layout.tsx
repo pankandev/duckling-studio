356f39b5768e207800e16aa950ebe1bcd7af4400
capaths <pcampaa@gmail.com>
2025-05-08 02:27:34 -0400

    feat: add home button to datasets layout

--- apps/web/src/app/text-classification/layout.tsx
+++ apps/web/src/app/text-classification/layout.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Link from "next/link";
+import {Home} from "lucide-react";
+import {Button} from "@/components/ui/button";
 
-const TextClassificationLayout = ({children}: {children: React.ReactNode}) => {
+const TextClassificationLayout = ({children}: { children: React.ReactNode }) => {
     return (
         <div className="flex flex-col items-stretch w-full gap-2">
             <div className="flex flex-row items-center justify-between py-3 px-5">
-                <div className="flex flex-row">
+                <div className="flex flex-row gap-2 items-center">
+                    <Button asChild variant="ghost" className="text-sm flex flex-row justify-between items-center">
+                        <Link href={('/')}>
+                            <Home size={16}></Home>
+                            <span>
+                                Home
+                            </span>
+                        </Link>
+                    </Button>
                     <h1 className="font-bold">Text Classification</h1>
                 </div>
             </div>
